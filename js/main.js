d7f80b18d394de97759bb4ef7c52e5e27fcca574
Dominika <d.szafraniec1404@gmail.com>
2024-02-09 15:38:34 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 const btn = document.querySelector('.nav__btn')
 const nav = document.querySelector('.nav__box')
+const allNavItems = document.querySelectorAll('.nav__item')
 
 const toggleNav = () => {
   nav.classList.toggle('active')
   document.body.classList.toggle('sticky-body')
+
+  hideNav()
+  
 }
+
+const hideNav = () => {
+  allNavItems.forEach(item => {
+    item.addEventListener('click', toggleNav)
+  });
+}
+
+
 
 btn.addEventListener('click', toggleNav)
